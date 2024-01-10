5b90cb83074e534ef2910ddbf14016d2415f0948
yangshaojun <yangshaojun@zhidaoauto.com>
2024-01-10 14:58:08 +0800

    kNN近邻算法

--- chapter02/kNN.py
+++ chapter02/kNN.py
@@ -9,6 +9,15 @@ import matplotlib.lines as mlines
 from sklearn.neighbors import KNeighborsClassifier as kNN
 
 
+# 影片分类数据集
+def createDataSet():
+    # 六组二维特征
+    group = array([[3, 104], [2, 100], [1, 81], [101, 10], [99, 5], [98, 2]]) # 使用numpy科学计算包模块中的array创建矩阵
+    # 六组特征的标签
+    labels = ['爱情片', '爱情片', '爱情片', '动作片', '动作片', '动作片']
+    return group, labels
+
+
 """
 函数说明:kNN算法,分类器
 Parameters:
@@ -22,18 +31,19 @@ Modify:
     2020-06-24
 """
 def classify0(inX, dataSet, labels, k):
-    dataSetSize = dataSet.shape[0]  # 矩阵的大小
-    diffMat = tile(inX, (dataSetSize, 1)) - dataSet  # https://www.cnblogs.com/zibu1234/p/4210521.html
-    sqDiffMat = diffMat ** 2
-    sqlDistances = sqDiffMat.sum(axis=1)  # 对于二维数组 axis=1表示按行相加 , axis=0表示按列相加
-    distances = sqlDistances ** 0.5
+    dataSetSize = dataSet.shape[0]  # 矩阵的大小,numpy函数shape[0]返回dataSet的行数
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet  # 在列向量方向上重复inX共1次(横向)，行向量方向上重复inX共dataSetSize次(纵向)
+    sqDiffMat = diffMat ** 2     # 二维特征相减后平方
+    sqlDistances = sqDiffMat.sum(axis=1)  # sum()所有元素相加，sum(0)列相加，sum(1)行相加
+    distances = sqlDistances ** 0.5     # 开方，计算出距离
     sortedDistIndicies = distances.argsort()  # 距离最小的样本从小到大排序 返回数据索引值
     classCount = {}  # 定义一个字典
     for i in range(k):
         votellable = labels[sortedDistIndicies[i]]  # 获取排序后，前k个样本的label
         classCount[votellable] = classCount.get(votellable, 0) + 1  # 指定默认值为0
         # 根据字典的第二个值进行排序 从大到小 python2 API为 classCount.iteritems() python3为classCount.items()
-    SortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+        # key=operator.itemgetter(1)表示按照字典的value进行排序；reverse=True为降序排序，
+        SortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return SortedClassCount[0][0]  # 取出第一个元素的key值
 
 
@@ -96,9 +106,9 @@ def showdatas(datingDataMat, datingLabels):
     # 画出散点图,以datingDataMat矩阵的第一(飞行常客例程)、第二列(玩游戏)数据画散点数据,散点大小为15,透明度为0.5
     axs[0][0].scatter(x=datingDataMat[:, 0], y=datingDataMat[:, 1], color=LabelsColors, s=15, alpha=.5)
     # 设置标题,x轴label,y轴label
-    axs0_title_text = axs[0][0].set_title(u'每年获得的飞行常客里程数与玩视频游戏所消耗时间占比', FontProperties=font)
-    axs0_xlabel_text = axs[0][0].set_xlabel(u'每年获得的飞行常客里程数', FontProperties=font)
-    axs0_ylabel_text = axs[0][0].set_ylabel(u'玩视频游戏所消耗时间占', FontProperties=font)
+    axs0_title_text = axs[0][0].set_title(u'每年获得的飞行常客里程数与玩视频游戏所消耗时间占比', fontproperties=font)
+    axs0_xlabel_text = axs[0][0].set_xlabel(u'每年获得的飞行常客里程数', fontproperties=font)
+    axs0_ylabel_text = axs[0][0].set_ylabel(u'玩视频游戏所消耗时间占', fontproperties=font)
     plt.setp(axs0_title_text, size=9, weight='bold', color='red')
     plt.setp(axs0_xlabel_text, size=7, weight='bold', color='black')
     plt.setp(axs0_ylabel_text, size=7, weight='bold', color='black')
@@ -106,9 +116,9 @@ def showdatas(datingDataMat, datingLabels):
     # 画出散点图,以datingDataMat矩阵的第一(飞行常客例程)、第三列(冰激凌)数据画散点数据,散点大小为15,透明度为0.5
     axs[0][1].scatter(x=datingDataMat[:, 0], y=datingDataMat[:, 2], color=LabelsColors, s=15, alpha=.5)
     # 设置标题,x轴label,y轴label
-    axs1_title_text = axs[0][1].set_title(u'每年获得的飞行常客里程数与每周消费的冰激淋公升数', FontProperties=font)
-    axs1_xlabel_text = axs[0][1].set_xlabel(u'每年获得的飞行常客里程数', FontProperties=font)
-    axs1_ylabel_text = axs[0][1].set_ylabel(u'每周消费的冰激淋公升数', FontProperties=font)
+    axs1_title_text = axs[0][1].set_title(u'每年获得的飞行常客里程数与每周消费的冰激淋公升数', fontproperties=font)
+    axs1_xlabel_text = axs[0][1].set_xlabel(u'每年获得的飞行常客里程数', fontproperties=font)
+    axs1_ylabel_text = axs[0][1].set_ylabel(u'每周消费的冰激淋公升数', fontproperties=font)
     plt.setp(axs1_title_text, size=9, weight='bold', color='red')
     plt.setp(axs1_xlabel_text, size=7, weight='bold', color='black')
     plt.setp(axs1_ylabel_text, size=7, weight='bold', color='black')
@@ -116,9 +126,9 @@ def showdatas(datingDataMat, datingLabels):
     # 画出散点图,以datingDataMat矩阵的第二(玩游戏)、第三列(冰激凌)数据画散点数据,散点大小为15,透明度为0.5
     axs[1][0].scatter(x=datingDataMat[:, 1], y=datingDataMat[:, 2], color=LabelsColors, s=15, alpha=.5)
     # 设置标题,x轴label,y轴label
-    axs2_title_text = axs[1][0].set_title(u'玩视频游戏所消耗时间占比与每周消费的冰激淋公升数', FontProperties=font)
-    axs2_xlabel_text = axs[1][0].set_xlabel(u'玩视频游戏所消耗时间占比', FontProperties=font)
-    axs2_ylabel_text = axs[1][0].set_ylabel(u'每周消费的冰激淋公升数', FontProperties=font)
+    axs2_title_text = axs[1][0].set_title(u'玩视频游戏所消耗时间占比与每周消费的冰激淋公升数', fontproperties=font)
+    axs2_xlabel_text = axs[1][0].set_xlabel(u'玩视频游戏所消耗时间占比', fontproperties=font)
+    axs2_ylabel_text = axs[1][0].set_ylabel(u'每周消费的冰激淋公升数', fontproperties=font)
     plt.setp(axs2_title_text, size=9, weight='bold', color='red')
     plt.setp(axs2_xlabel_text, size=7, weight='bold', color='black')
     plt.setp(axs2_ylabel_text, size=7, weight='bold', color='black')
@@ -317,30 +327,28 @@ def handwritingClassTest_sklearn():
 
 
 if __name__ == '__main__':
-    # group, labels = createDataSet()  # 创建数据集
-    # ret = classify0([0, 0], group, labels, 3)  # 返回分类标签
-    # print(ret)
-    # datingDataMat, datingLabels = file2matrix("./data/datingTestSet2.txt")
+    group, labels = createDataSet()  # 创建数据集
+    # 测试集
+    test = [18, 90]
+    # KNN分类
+    ret = classify0(test, group, labels, 3)  # 返回分类标签
+    # 打印分类结果
+    print(ret)
+
+    datingDataMat, datingLabels = file2matrix("./data/datingTestSet2.txt")
     # 数据可视化
-    # showdatas(datingDataMat, datingLabels)
-    # normDataSet, ranges, minVals = autoNorm(datingDataMat)
-    # print(normDataSet)
-    # print(ranges)
-    # print(minVals)
-    # datingClassTest()
-    # classifyPerson()
-    # handwritingClassTest()
-    handwritingClassTest_sklearn()
+    showdatas(datingDataMat, datingLabels)
+
     # matplotlib 展示
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111)
-    # # ax.scatter(datingDataMat[:,1],datingDataMat[:,2])
-    # ax.scatter(datingDataMat[:, 0], datingDataMat[:, 1], 15.0 * array(datingLabels), 15.0 * array(datingLabels))
-    # plt.show()
-    # # 数据归一化
-    # normDataSet, ranges, minValues = autoNorm(datingDataMat)
-    # # print(normDataSet)
-    # datingClassTest()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    # ax.scatter(datingDataMat[:,1],datingDataMat[:,2])
+    ax.scatter(datingDataMat[:, 0], datingDataMat[:, 1], 15.0 * array(datingLabels), 15.0 * array(datingLabels))
+    plt.show()
+
+    normDataSet, ranges, minVals = autoNorm(datingDataMat)
+    datingClassTest()
     # classifyPerson()
-    # img2vector("/Users/yangshaojun/python_workspace/chapter02/trainingDigits/0_13.txt")
-    # handwritingClassTest()
+    handwritingClassTest()
+    handwritingClassTest_sklearn()
+
